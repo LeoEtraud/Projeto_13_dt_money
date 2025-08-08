5d950b539e7d0a80d8de5b879ea0aab4630db2e6
Leonardo Duarte <leocc_etraud@outlook.com>
2025-08-08 17:59:48 -0300

    fix: mais um ajuste na requisição de busca a API

--- src/contexts/transactionProvider/util.ts
+++ src/contexts/transactionProvider/util.ts
@@ -16,7 +16,7 @@ export async function fetchTransaction(query?: string) {
   try {
     const request = await apiDtMoney.get("/transactions", {
       params: {
-        _sort: "createdAt",
+        _sort: "id",
         _order: "desc",
         q: query, // pesquisa por query se existir
         page: 0,
